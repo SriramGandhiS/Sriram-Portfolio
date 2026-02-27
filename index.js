dd0bd5e3388ceaa4ded1db6b00990ee495dd56a1
Sriramgandhi <iamramm8@gmail.com>
2026-02-27 22:51:00 +0530

    feat: Add download button and translucent borders to cert tiles

--- index.js
+++ index.js
@@ -205,6 +205,13 @@ function openParallaxView(companyName) {
       <div class="cert-bento-label">
         <span class="cert-index">${index + 1} / ${certs.length}</span>
         <span class="cert-name">${cert.name}</span>
+        <a href="${cert.file.split('#')[0]}" download class="cert-download-btn" title="Download">
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+            <polyline points="7 10 12 15 17 10"></polyline>
+            <line x1="12" y1="15" x2="12" y2="3"></line>
+          </svg>
+        </a>
       </div>
     `;
 
